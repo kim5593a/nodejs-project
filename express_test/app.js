df05dfdaf2573414bee324f293e4d59a1f52bff0
kim5593a <kim8745a@gmail.com>
2022-03-16 18:05:17 +0900

    Upload

--- express_test/app.js
+++ express_test/app.js
@@ -4,6 +4,7 @@ const express =require('express'); //express 패키지 import
 const path=require('path');//경로 패키지 import
 const morgan =require('morgan'); //morgan 패키지 import
 const cookieParser=require('cookie-parser');//cookieParser 패키지 import
+const request=require('request');//API 요청 패키지 import
 //---------------------------------------------------------------
 //---------------------------------------------------------------
 //[Server 세팅]
@@ -22,34 +23,48 @@ app.use((req,res,next)=>{
     //실행 했으면 라우터 이동
     next();
 });
-//Server에서 /get 라우터 전달 시 동작하는 Code
-app.use('/get',(req,res,next)=>{
-    console.log('get에서만 작동하는 미들웨어');
-    //실행 했으면 라우터 이동
-    next();
-});
+// //Server에서 /get 라우터 전달 시 동작하는 Code
+// app.use('/get',(req,res,next)=>{
+//     console.log('get에서만 작동하는 미들웨어');
+//     //실행 했으면 라우터 이동
+//     next();
+// });
 //---------------------------------------------------------------
 //---------------------------------------------------------------
 //defult reqest
-//http://localhost:3000/
+//http://localhost:3000/-----------------------------------------------
+//GIB-API Address:https://sgapi.group-ib.com/v2.1/events/bylogin?key=ECQ6BDihHQPPHfbSOpRD6WIQrE3zEefoqBNlNQ81rYLu3jR0&login=b9526479d8e75eec48092197447318d86d2194ec
 app.get('/test',(req,res)=>{
     req.cookies//사용자 쿠키 받아오기
     //쿠키 만들어서 보내기
-    res.cookie('name',encodeURIComponent(name),{
-        expires:new Date(),
-        httpOnly:true,
-        path:'/',
-    })
-    //쿠키 제거
-    res.clearCookie('name',encodeURIComponent(name),{
-        httpOnly:true,
-        path:'/',
-    })
-    res.send('get express');
+    // res.cookie('name',encodeURIComponent(name),{
+    //     expires:new Date(),
+    //     httpOnly:true,
+    //     path:'/',
+    // })
+    // //쿠키 제거
+    // res.clearCookie('name',encodeURIComponent(name),{
+    //     httpOnly:true,
+    //     path:'/',
+    // })
+
+    //GIB API Json 요청----------------------------------------
+    var getUrl='https://sgapi.group-ib.com/v2.1/events/bylogin?key=ECQ6BDihHQPPHfbSOpRD6WIQrE3zEefoqBNlNQ81rYLu3jR0&login=b9526479d8e75eec48092197447318d86d2194ec'
+    request.put({
+        headers:{'content-type':'application/json'},
+        url:getUrl,
+        json:true
+    },function(error,response,body){
+        var data=res.json(JSON.parse(body));
+        console.log('GIB API 값 확인: ',data);
+    });
+    // res.send('get express');
 });
+//------------------------------------------------------------------------
 app.post('/post',(req,res)=>{
     //res.writeHead(200,{'Content-Type':'application/json'});
     //Json 값 보낼때 Head
+    console.log('Post Test');
     res.json({json_data:'Data_String'})
 });
 app.put('/put',(req,res)=>{
